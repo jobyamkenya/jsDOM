5c9370c16ae4391af23a3b6290ff8000e4776bad
Job Ya Mkenya <jobyamkenya@gmail.com>
2018-03-19 19:36:49 +0300

    div

--- AppendToDom/append.js
+++ AppendToDom/append.js
@@ -2,3 +2,10 @@ var h1 = document.createElement('H1');
 var h1Text = document.createTextNode("HERE FROM JS BABY");
 h1.appendChild(h1Text);
 document.body.appendChild(h1);
+
+
+var myDiv = document.createElement('div');
+myDiv.setAttribute('id', 'container');
+myDiv.setAttribute('color', 'blue');
+document.body.appendChild('myDiv');
+
